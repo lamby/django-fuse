fcbaa96894264f79f875ad35a1bc0c0fdb11fd36
Chris Lamb <lamby@debian.org>
2008-12-29 17:50:36 +0000

    Add example application.
    Signed-off-by: Chris Lamb <lamby@debian.org>

---
+++ django_fuse_example/example/urls.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# django-fuse -- FUSE adaptor for Django
+# Copyright (C) 2008 Chris Lamb <chris@chris-lamb.co.uk>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.conf.urls.defaults import *
+
+import views
+
+urlpatterns = patterns('',
+   url(r'^$', views.index),
+   url(r'^hostname\.txt$', views.hostname),
+   url(r'^subdir$', views.subdir),
+   url(r'^subdir/(a|b|c)\.txt$', views.letter_file),
+)
---
+++ django_fuse_example/example/views.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+# django-fuse -- FUSE adaptor for Django
+# Copyright (C) 2008 Chris Lamb <chris@chris-lamb.co.uk>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from django_fuse import DirectoryResponse, FileResponse, WrappedFileResponse
+
+def index():
+    """
+    Demonstrating a simple list-based directory index.
+    """
+
+    items = ['hostname.txt', 'subdir']
+    return DirectoryResponse(items)
+
+def hostname():
+    """
+    Demonstrating wrapping a request to a "real" file on the filesystem.
+    """
+
+    return WrappedFileResponse('/etc/hostname')
+
+def subdir():
+    """
+    Demonstrating a generator-based approach to populating a directory
+    index. The use of a count callback function is an optimisation to
+    prevent evaluation of the generator when displaying the file in a
+    directory index.
+
+    Imagine replacing these static results with ones based on a QuerySet-
+    based database query. You may wish to use itertools.imap to lazily
+    format the filename before returning it.
+    """
+
+    def items():
+        for letter in "abc":
+            yield "%s.txt" % letter
+
+    def count_cb():
+        return 3
+
+    return DirectoryResponse(items, count_cb)
+
+def letter_file(letter):
+    """
+    Demonstrating using the Django templating system to create a faux
+    file with dynamic contents.
+    """
+
+    from django.template import Context, Template
+
+    t = Template("""This is {{ letter }}.txt\n""")
+    c = Context({'letter': letter})
+
+    return FileResponse(t.render(c))
