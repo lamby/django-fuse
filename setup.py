a4bf5b0340d5845fb8c40119301f9714548e7ead
Chris Lamb <lamby@debian.org>
2008-12-30 21:38:47 +0000

    Add a setup.py
    Signed-off-by: Chris Lamb <lamby@debian.org>

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='django_fuse',
+    packages=[
+        'django_fuse',
+        'django_fuse.management',
+        'django_fuse.management.commands',
+    ],
+)
